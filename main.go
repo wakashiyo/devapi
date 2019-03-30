ca2d79d1fe9d59796ca65e5f2357823a53037826
takahiroyoshikawa <aihasayahukoma7@gmail.com>
2019-03-30 15:52:40 +0900

    add: comment

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"net/http"
 )
 
+//handler : http request handler, return "hello world"
 func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hello, World !$%&!%&")
+	fmt.Fprintf(w, "Hello, World !!!!!!!!!!!!!!!!!!!")
 }
 
 func main() {
-  http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
-  http.ListenAndServe(":9001", nil)
+	http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
+	http.ListenAndServe(":9001", nil)
 }
